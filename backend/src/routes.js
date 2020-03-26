b882132af050bf5bb5096a296df9eb4f972e7164
brenndafarinha <brennda.perola@gmail.com>
2020-03-26 16:30:21 -0300

    add app backend

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
+
+const routes = express.Router()
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OngController.list)
+routes.post('/ongs', OngController.create)
+
+routes.get('/profile', ProfileController.list)
+
+routes.get('/incidents', IncidentController.list)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
+
+
+module.exports = routes
